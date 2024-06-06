ef3e917cd0cad5410bdb96e3566a75d1d37f4901
Gui Caçador <gxavier@esapiens.com.br>
2024-06-05 23:02:00 -0300

    Remove count

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Head from "./components/Head";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<section className="flex items-center justify-center h-svh -z-20">
 			<Head />
